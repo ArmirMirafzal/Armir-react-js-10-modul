37992030257240ba23f6a5b828eb53bd6bbd6306
Armir <mirarmirkhan@gmail.com>
2023-08-18 09:12:32 +0500

    next commit

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAW3eT87XVz_orHL8oHYaXb7QLd85_66zM",
+    authDomain: "armir-chess-project-auth.firebaseapp.com",
+    projectId: "armir-chess-project-auth",
+    storageBucket: "armir-chess-project-auth.appspot.com",
+    messagingSenderId: "665484076437",
+    appId: "1:665484076437:web:cb0db05da6f2ad3ba2829c",
+    measurementId: "G-9D5K30XMM2",
+  },
+};
